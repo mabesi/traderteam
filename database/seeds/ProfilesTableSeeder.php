f40ed5b9fc65fcb4ea40a2e80b4eef4379f5e1f3
Plinio Mabesi <pliniomabesi@gmail.com>
2017-09-12 14:27:47 -0300

    Ajuste avatar novamente

--- database/seeds/ProfilesTableSeeder.php
+++ database/seeds/ProfilesTableSeeder.php
@@ -25,6 +25,7 @@ class ProfilesTableSeeder extends Seeder
         'facebook' => 'https://www.facebook.com/plinio.mabesi',
         'twitter' => 'https://www.twitter.com/pliniomabesi',
         'description' => 'Esta é a minha descrição dizendo um pouco sobre mim.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla gravida lorem turpis, vitae porta leo congue ut.<br> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean vitae imperdiet tortor. Donec consectetur velit eu lectus sodales pulvinar. Vestibulum at egestas ex, et accumsan quam. Suspendisse convallis diam nibh, convallis sodales eros aliquam id. Cras ut lectus eu nisi tincidunt dignissim sed eget mauris.',
+        'sidebar_closed' => True,
         'created_at' => date("Y-m-d H:i:s"),
         'updated_at' => date("Y-m-d H:i:s"),
       ]);
@@ -39,6 +40,7 @@ class ProfilesTableSeeder extends Seeder
         'facebook' => 'https://www.facebook.com/liza.barral',
         'twitter' => 'https://www.twitter.com/lizabarral',
         'description' => 'Esta é a minha descrição dizendo um pouco sobre mim.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla gravida lorem turpis, vitae porta leo congue ut.<br> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean vitae imperdiet tortor. Donec consectetur velit eu lectus sodales pulvinar. Vestibulum at egestas ex, et accumsan quam. Suspendisse convallis diam nibh, convallis sodales eros aliquam id. Cras ut lectus eu nisi tincidunt dignissim sed eget mauris.',
+        'sidebar_closed' => True,
         'created_at' => date("Y-m-d H:i:s"),
         'updated_at' => date("Y-m-d H:i:s"),
       ]);
@@ -53,6 +55,7 @@ class ProfilesTableSeeder extends Seeder
         'facebook' => 'https://www.facebook.com/eng.civil',
         'twitter' => 'https://www.twitter.com/engcivil',
         'description' => 'Esta é a minha descrição dizendo um pouco sobre mim.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla gravida lorem turpis, vitae porta leo congue ut.<br> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean vitae imperdiet tortor. Donec consectetur velit eu lectus sodales pulvinar. Vestibulum at egestas ex, et accumsan quam. Suspendisse convallis diam nibh, convallis sodales eros aliquam id. Cras ut lectus eu nisi tincidunt dignissim sed eget mauris.',
+        'sidebar_closed' => True,
         'created_at' => date("Y-m-d H:i:s"),
         'updated_at' => date("Y-m-d H:i:s"),
       ]);
@@ -67,6 +70,7 @@ class ProfilesTableSeeder extends Seeder
         'facebook' => 'https://www.facebook.com/manicure',
         'twitter' => 'https://www.twitter.com/manicure',
         'description' => 'Esta é a minha descrição dizendo um pouco sobre mim.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla gravida lorem turpis, vitae porta leo congue ut.<br> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean vitae imperdiet tortor. Donec consectetur velit eu lectus sodales pulvinar. Vestibulum at egestas ex, et accumsan quam. Suspendisse convallis diam nibh, convallis sodales eros aliquam id. Cras ut lectus eu nisi tincidunt dignissim sed eget mauris.',
+        'sidebar_closed' => True,
         'created_at' => date("Y-m-d H:i:s"),
         'updated_at' => date("Y-m-d H:i:s"),
       ]);
@@ -81,6 +85,7 @@ class ProfilesTableSeeder extends Seeder
         'facebook' => 'https://www.facebook.com/bancario',
         'twitter' => 'https://www.twitter.com/bancario',
         'description' => 'Esta é a minha descrição dizendo um pouco sobre mim.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla gravida lorem turpis, vitae porta leo congue ut.<br> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean vitae imperdiet tortor. Donec consectetur velit eu lectus sodales pulvinar. Vestibulum at egestas ex, et accumsan quam. Suspendisse convallis diam nibh, convallis sodales eros aliquam id. Cras ut lectus eu nisi tincidunt dignissim sed eget mauris.',
+        'sidebar_closed' => True,
         'created_at' => date("Y-m-d H:i:s"),
         'updated_at' => date("Y-m-d H:i:s"),
       ]);
